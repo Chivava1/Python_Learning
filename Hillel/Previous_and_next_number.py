9ddb79ca6b1e61ef0cdb552c76d98ece11f25895
VShpolskyi <shpolskiy.v@gmail.com>
2023-09-21 01:42:10 +0300

    Prev_and_next_initial_comm

---
+++ Hillel/Previous_and_next_number.py
@@ -0,0 +1,7 @@
+#How to find out the next and previous number from a given one..
+number = int(input("Input your number: "))
+number_plus_one = number + 1
+number_minus_one = number - 1
+print("The previous number to your number is: ", number_minus_one)
+print("The next number to your number is: ", number_plus_one)
+
