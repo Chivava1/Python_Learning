ad28bebde891891d5f3fe19865dcf990e5d44067
VShpolskyi <shpolskiy.v@gmail.com>
2023-09-21 01:48:53 +0300

    Trngl_sqr_initial_comm

---
+++ Hillel/Triangle_Square.py
@@ -0,0 +1,9 @@
+#How to to calculate Right Triangle square
+print("\033[1mHi! I'll help you to calculate Right Triangle square, please follow instructions below.\033[0m")
+
+cat1 = float(input("Input triangle Length side 1: "))
+cat2 = float(input("Input triangle Length side 2: "))
+
+S = float(0.5)*cat1*cat2
+
+print("The square of your triangle is :", round(S, 2), "cm²")
